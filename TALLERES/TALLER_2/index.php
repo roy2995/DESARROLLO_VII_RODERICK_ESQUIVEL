f607c94f35ad93c7201883bb1cb8bfeb617e4411
Roederick Esquivel <roderickenrique42@gmail.com>
2024-08-25 22:44:50 -0500

    Definidas variables de diferentes tipos

--- TALLERES/TALLER_2/index.php
+++ TALLERES/TALLER_2/index.php
@@ -1,4 +1,12 @@
 
 <?php
+$name = "Juan";
+$age = 25;
+$hight = 1.75;
+$itsStudent = true;
 
+echo "Name: $name<br>";
+echo "Age: $age<br>";
+echo "Hight: $hight<br>";
+echo "It's student? " . ($itsStudent ? "Sí" : "No");
 ?>
